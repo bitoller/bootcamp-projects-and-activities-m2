a848ced729df1355ecad86872cde8f06969ec189
bitoller <bi_toller@hotmail.com>
2023-01-24 19:24:24 -0300

    changed files style

---
+++ src/M2-SPRINT-1/S1-13-M2/scripts/modal.js
@@ -0,0 +1,9 @@
+function modal() {
+  const modalContainer = document.querySelector(".modal-container");
+  modalContainer.showModal();
+}
+
+function closeModal() {
+  const modalContainer = document.querySelector(".modal-container");
+  modalContainer.close();
+}
