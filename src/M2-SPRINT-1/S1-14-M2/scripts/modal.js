5044e18856f02cc5657af732f4fd721883654606
bitoller <bi_toller@hotmail.com>
2023-01-25 21:42:54 -0300

    done

---
+++ src/M2-SPRINT-1/S1-14-M2/scripts/modal.js
@@ -0,0 +1,22 @@
+function handleModal() {
+  const buttonHeader = document.querySelector("#openModalHeader");
+  const buttonMain = document.querySelector("#openModalMain");
+  buttonHeader.addEventListener("click", openModal);
+  buttonMain.addEventListener("click", openModal);
+}
+
+function openModal() {
+  const modalContainer = document.querySelector("#modalContainer");
+  modalContainer.showModal();
+}
+
+function closeModal() {
+  const buttonCloseModal = document.querySelector("#closeModal");
+  const modalContainer = document.querySelector("#modalContainer");
+  buttonCloseModal.addEventListener("click", () => {
+    modalContainer.close();
+  });
+}
+
+handleModal();
+closeModal();
