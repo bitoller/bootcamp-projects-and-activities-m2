b308534da08d682d4fb1210886a9851b9930b788
bitoller <bi_toller@hotmail.com>
2023-01-26 16:40:43 -0300

    swapped onclick for eventlistener and added colors

--- src/M2-SPRINT-1/S1-13-M2/scripts/modal.js
+++ src/M2-SPRINT-1/S1-13-M2/scripts/modal.js
@@ -1,9 +1,21 @@
-function modal() {
+function handleModal() {
+  const buttonModal = document.querySelector(".btn-cadastrar");
+  buttonModal.addEventListener("click", openModal);
+}
+
+function openModal() {
   const modalContainer = document.querySelector(".modal-container");
   modalContainer.showModal();
+
+  closeModal();
 }
 
 function closeModal() {
+  const buttonCloseModal = document.querySelector(".close-modal");
   const modalContainer = document.querySelector(".modal-container");
-  modalContainer.close();
+  buttonCloseModal.addEventListener("click", () => {
+    modalContainer.close();
+  });
 }
+
+handleModal();
