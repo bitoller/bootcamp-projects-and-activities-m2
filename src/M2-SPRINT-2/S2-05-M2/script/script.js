0033922ab5b31ca7d738e4678b4b0b029279ef3d
bitoller <bi_toller@hotmail.com>
2023-01-30 15:32:42 -0300

    s2-05 done

--- src/M2-SPRINT-2/S2-05-M2/script/script.js
+++ src/M2-SPRINT-2/S2-05-M2/script/script.js
@@ -1,28 +1,39 @@
 //1
 function filterCountry(array) {
-  return;
+  let filteredCountry = array.filter(
+    (country) => country.country.toLowerCase() == "brasil"
+  );
+  return filteredCountry;
 }
 console.log(filterCountry(data));
 
 //2
 function filterPrice(array) {
-  return;
+  let filteredPrice = array.filter((price) => price.price >= 200);
+  return filteredPrice;
 }
 console.log(filterPrice(data));
 
 //3
 function filterIsOpen(array) {
-  return;
+  let filteredOpen = array.filter((open) => open.isOpen == true);
+  return filteredOpen;
 }
 console.log(filterIsOpen(data));
 
 //DESAFIO
 function filterHotelName(array) {
-  return;
+  let filteredName = array.filter(
+    (name) => name.name.toLowerCase() == "copacabana palace"
+  );
+  return filteredName[0];
 }
 
 function filterToBook() {
   const hotel = filterHotelName(data);
-  return;
+  let isAvaliable = hotel.toBook.filter(
+    (avaliable) => avaliable.isAvaliable == true
+  );
+  return isAvaliable;
 }
 console.log(filterToBook());
