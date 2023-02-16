00727424b98f34edf96465e6444366d05553dd42
bitoller <bi_toller@hotmail.com>
2023-02-16 17:12:21 -0300

    done

--- src/M2-SPRINT-3/S3-03-M2/scripts/script.js
+++ src/M2-SPRINT-3/S3-03-M2/scripts/script.js
@@ -1,7 +1,19 @@
 function menuButton() {
   const button = document.querySelector(".button-open");
-  button.addEventListener("click", () =>
-    document.querySelector(".list-show").classList.toggle("show")
-  );
+  button.addEventListener("click", () => {
+    let list = document.querySelector(".list-show");
+    list.classList.toggle("show");
+    let search = document.querySelector(".search").classList.toggle("show");
+    let img = document.querySelector(".img-show").classList.toggle("show");
+    let username = document
+      .querySelector(".user-name")
+      .classList.toggle("show");
+    let btnImg = document.querySelector(".menu-img");
+    if (list.classList.contains("show") == true) {
+      btnImg.src = "./assets/close-button.svg";
+    } else {
+      btnImg.src = "./assets/menu-button.svg";
+    }
+  });
 }
 menuButton();
