57d0951a9edacf0cd4c5d586f67a666adae65360
bitoller <bi_toller@hotmail.com>
2023-02-23 19:30:12 -0300

    removed activity

--- src/M2-SPRINT-5/S5-04-M2/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import(".prisma/client").Prisma.RejectOnNotFound | import(".prisma/client").Prisma.RejectPerOperation | undefined>;
-export declare const app: import("express-serve-static-core").Express;
--- src/M2-SPRINT-5/S5-04-M2/dist/routes/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const routes: import("express-serve-static-core").Router;
