9ae65a4b5d7ca9a2910694d571867a86aebe8ea8
bitoller <bi_toller@hotmail.com>
2023-03-10 16:41:46 -0300

    fix path

--- src/M2-SPRINT-7/S7-01-M2/front-end/scripts/home.js
+++ src/M2-SPRINT-7/S7-01-M2/front-end/scripts/home.js
@@ -26,11 +26,11 @@ function buttons() {
 }
 
 function signInRedirect() {
-  window.location.replace("../pages/signIn.html");
+  window.location.replace("./pages/signIn.html");
 }
 
 function signUpRedirect() {
-  window.location.replace("../pages/signUp.html");
+  window.location.replace("./pages/signUp.html");
 }
 
 async function getSectors() {
