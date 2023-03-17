88afdf8fbdfe5dcc27a0a04f3b93406986b9d3d7
bitoller <bi_toller@hotmail.com>
2023-03-17 19:42:56 -0300

    progress

---
+++ src/M2-SPRINT-5/S5-14-M2/scripts/index.js
@@ -0,0 +1,110 @@
+import { getAll, getOne } from "./pokemon.js";
+
+let pageNumber = 0;
+let defaultUrlImg =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+let gifUrlImg =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/";
+
+async function showSelectedPokemon(name, image, id) {
+  let pokemon = await getOne(name);
+  let pokedexTop = document.querySelector("#pokedex-top");
+  pokedexTop.innerHTML = "";
+
+  let pokemonNameAndId = document.createElement("h1");
+  pokemonNameAndId.innerText = `${name} #${pokemon.id}`;
+
+  let topContainer = document.createElement("div");
+  topContainer.classList.add("pokedex-container");
+
+  let containerLeft = document.createElement("div");
+  containerLeft.classList.add("container-left");
+
+  let heightAndWeightDiv = document.createElement("div");
+  heightAndWeightDiv.classList.add("height-weight");
+
+  let height = document.createElement("p");
+  height.innerText = `${pokemon.height} m`; //???????????
+
+  let weight = document.createElement("p");
+  weight.innerText = `${pokemon.weight} kg`; //???????????
+
+  let imgDiv = document.createElement("div");
+  imgDiv.classList.add("img-div");
+
+  let pokemonImg = document.createElement("img");
+  pokemonImg.src = `${gifUrlImg}${id}.gif`;
+  pokemonImg.addEventListener("error", () => {
+    pokemonImg.src = image;
+  });
+
+  let containerRight = document.createElement("div");
+  containerRight.classList.add("container-right");
+
+  let descriptionTitle = document.createElement("h5");
+  descriptionTitle.innerText = "Description";
+
+  let descriptionContent = document.createElement("p");
+  descriptionContent.innerText = pokemon.flavor_text_entries.flavor_text;
+
+  heightAndWeightDiv.append(height, weight);
+  imgDiv.append(pokemonImg);
+  containerLeft.append(heightAndWeightDiv, imgDiv);
+  containerRight.append(descriptionTitle, descriptionContent);
+  topContainer.append(containerLeft, containerRight);
+  pokedexTop.append(pokemonNameAndId, topContainer);
+}
+
+function createPokemon(image, name, id) {
+  let pokemon = document.createElement("li");
+
+  let pokemonButton = document.createElement("button");
+  pokemonButton.addEventListener("click", async () => {
+    await showSelectedPokemon(name, image, id);
+  });
+
+  let pokemonImg = document.createElement("img");
+  pokemonImg.src = image;
+  pokemonImg.setAttribute("loading", "lazy");
+
+  pokemonButton.append(pokemonImg);
+  pokemon.append(pokemonButton);
+
+  return pokemon;
+}
+
+async function renderAll(pageNumber = 0) {
+  let pokemons = await getAll(pageNumber);
+  let pokemonList = document.querySelector("#pokemon-list");
+  pokemonList.innerHTML = "";
+
+  pokemons.results.forEach((pokemon) => {
+    let pokemonId = pokemon.url.slice(34, -1);
+    let pokemonCard = createPokemon(
+      `${defaultUrlImg}${pokemonId}.png`,
+      pokemon.name,
+      pokemonId
+    );
+
+    pokemonList.append(pokemonCard);
+  });
+}
+
+function browseButtons() {
+  let buttonUp = document.querySelector("#back-button");
+  buttonUp.addEventListener("click", async () => {
+    if (pageNumber > 0) {
+      pageNumber -= 21;
+      await renderAll(pageNumber);
+    }
+  });
+
+  let buttonDown = document.querySelector("#next-button");
+  buttonDown.addEventListener("click", async () => {
+    pageNumber += 21;
+    await renderAll(pageNumber);
+  });
+}
+
+renderAll();
+browseButtons();
---
+++ src/M2-SPRINT-5/S5-14-M2/scripts/pokemon.js
@@ -0,0 +1,34 @@
+const url = "https://pokeapi.co/api/v2/";
+
+export async function getAll(offset = 0) {
+  const options = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  let pokemons = {};
+  try {
+    pokemons = await fetch(`${url}pokemon?offset=${offset}&limit=21`, options);
+    return pokemons.json();
+  } catch (error) {}
+
+  return null;
+}
+
+export async function getOne(name) {
+  const options = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  let pokemon = {};
+  try {
+    pokemon = await fetch(`${url}pokemon-species/${name}`, options);
+    if (pokemon.status == 404) {
+      return null;
+    }
+    return pokemon.json();
+  } catch (error) {}
+
+  return null;
+}
